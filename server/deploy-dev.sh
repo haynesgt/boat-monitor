299b9d7e82d2d0915730073d87b933aaf241c168
Gavin Haynes <ghaynes@atb.com>
2020-07-26 08:55:03 -0700

    Better deploy

--- server/deploy-dev.sh
+++ server/deploy-dev.sh
@@ -1,3 +1,8 @@
 node -c ./index.js
-gcloud functions deploy --project haynes-boat-dev savePacket --runtime nodejs10 --trigger-http
-gcloud functions deploy --project haynes-boat-dev getPackets --runtime nodejs10 --trigger-http
+TAG=server-dev-$(date '+%s')
+git tag $TAG
+git push origin $TAG
+echo https://console.cloud.google.com/cloud-build/builds?project=haynes-boat-dev
+
+# gcloud functions deploy --project haynes-boat-dev savePacket --runtime nodejs10 --trigger-http
+# gcloud functions deploy --project haynes-boat-dev getPackets --runtime nodejs10 --trigger-http
